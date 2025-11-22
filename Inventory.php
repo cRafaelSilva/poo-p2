3cd354650cff0492083298359d61f21ba317b493
Rafael Casagrande <rafaehl.casagrande@gmail.com>
2025-11-22 15:50:47 -0300

    Implementando os métodos da Classe Inventory

--- Inventory.php
+++ Inventory.php
@@ -8,10 +8,10 @@ class Inventory
     /**@var Item[] */
     private array $itens;
 
-    public function __construct()
+    public function __construct($maxCapacity = 20)
     {
-        $this->setMaxCapacity();
-        $this->itens = [];
+        $this->SetmaxCapacity($maxCapacity);
+        $this->setItens();
     }
 
     public function getMaxCapacity(): int
@@ -19,8 +19,60 @@ class Inventory
         return $this->maxCapacity;
     }
 
-    public function setMaxCapacity(): void{
-        $extraCapacity = Player->getLevel() * 3;
-        $this->maxCapacity = 20  + $extraCapacity  
+    public function setMaxCapacity($maxCapacity): void
+    {
+        $this->maxCapacity = $maxCapacity;
+        return;
+    }
+
+    public function getItens(): array
+    {
+        return $this->itens;
+    }
+
+    public function setItens(): void
+    {
+        $this->itens = [];
+        return;
+    }
+
+    public function addItem(Item $item): bool
+    {
+        if ($item->getSize() <= $this->freeCapacit()) {
+            $this->itens[] = $item;
+            return true;
+        }
+        return false;
+    }
+
+    public function removeItem(Item $item): bool
+    {
+        $name = $item->getName();
+        $name = mb_strtolower($name);
+
+        foreach ($this->itens as $index => $item) {
+            if (mb_strtolower($item->getName()) == $name) {
+                array_splice($this->itens, $index, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function freeCapacit(): int
+    {
+        $occupiedSpace = 0;
+        foreach ($this->itens as $item) {
+            $occupiedSpace += $item->getSize();
+        }
+        $freeCapacit =  $this->maxCapacity - $occupiedSpace;
+        return $freeCapacit;
+    }
+
+    public function updateMaxCapacity($level)
+    {
+        $currentCapacity = $this->getMaxCapacity();
+        $newCapacity = $currentCapacity + ($level * 3);
+        $this->setMaxCapacity($newCapacity);
     }
 }
